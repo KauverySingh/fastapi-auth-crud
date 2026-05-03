be87738d0682a4cb1c6d12fd89f353fe56509fab
Kavy <ksstudent26@gmail.com>
2026-05-04 02:08:48 +0530

    Updated database.py

--- database.py
+++ database.py
@@ -1,10 +1,26 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DATABASE_URL = "postgresql://postgres:root@localhost/fastapi_db"
+# # DATABASE_URL = "postgresql://postgres:root@db/fastapi_db"
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(bind=engine)
+
+# Base = declarative_base()
+
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:root@localhost/fastapi_db"
-# DATABASE_URL = "postgresql://postgres:root@db/fastapi_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
